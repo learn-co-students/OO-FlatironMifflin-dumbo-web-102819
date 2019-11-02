09faa90886f92aa786c6e87087ce0d460d2f5465
github username <github email address>
2019-11-02 12:12:11 -0400

    Done.

--- lib/Employee.rb
+++ lib/Employee.rb
@@ -1,3 +1,37 @@
 class Employee
+    attr_accessor :salary 
+    attr_reader :name, :manager 
+    @@all = []
+    def initialize(name, salary, manager)
+        @name = name
+        @salary = salary
+        @manager = manager
+        @@all << self
+    end
 
+    def manager_name
+        self.manager.name
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.paid_over(amount)
+        all.select do |employee|
+            employee.salary > amount
+        end
+    end
+
+    def self.find_by_department(department)
+        all.find do |employee|
+            employee.manager.department == department
+        end
+    end
+
+    def tax_bracket
+        self.class.all.select do |employee|
+            employee.salary >= self.salary - 1_000 && employee.salary <= self.salary + 1_000
+        end
+    end
 end
--- run.rb
+++ run.rb
@@ -4,6 +4,19 @@ require 'pry'
 
 
 #Test your code here
+tim = Manager.new("Tim","IT",37)
+bob = Manager.new("Bob","Accounting",35)
+shiela = Manager.new("Shiela","Operations",65)
+
+max = Employee.new("Max",45_000,tim)
+deborah = Employee.new("Deborah",43_000,tim)
+denzel = Employee.new("denzel",46_000,bob)
+tristan = Employee.new("tristan",44_000,bob)
+pascal = Employee.new("pascal",59_000,shiela)
+marcus = Employee.new("marcus",57_000,shiela)
+
+
+
 
 
 binding.pry
