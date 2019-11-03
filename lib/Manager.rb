7dde315ea78188991b0c2a3a054db57ffd4b3366
Frank Gerold <FrankETSGerold@gmail.com>
2019-11-03 15:24:15 -0500

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,47 @@
 class Manager
 
+  attr_reader :name
+  attr_accessor :age, :department
+
+  @@all = []
+
+  def self.all
+    @@all
+  end
+
+  def initialize
+    @@all << self
+  end
+
+  def employees
+    Employee.all.select {|name| name.manager == self}
+  end
+
+  def hire_employees (name, salary)
+    Employee.all.map do
+      |employee|
+      if employee.name == name
+        employee.manager = self
+        employee.salary = salary
+      end
+    end
+  end
+
+  def self.all_departments
+    @@all.map do
+      |manager|
+      manager.department
+    end
+  end
+
+  def self.average_age
+    age_total = 0.0
+    age_list = @@all.map do {|man| man.age}
+    age_list.length.times {|list_age| age_total += list_age}
+    age_total /= age_list
+  end
+
+
+
+
 end
