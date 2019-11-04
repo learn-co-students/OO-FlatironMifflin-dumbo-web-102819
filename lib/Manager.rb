1d7efb455182ffb917d88c1cf331b94e22d4109f
FanZhong Zeng <fanzhongzeng78@gmail.com>
2019-11-04 12:50:56 -0500

    Done.

--- lib/Manager.rb
+++ lib/Manager.rb
@@ -1,3 +1,46 @@
 class Manager
+	attr_accessor :name, :department, :age
 
+	@@all = []
+
+	def initialize(name, department, age)
+		@name = name
+		@department = department
+		@age = age
+
+		@@all << self
+	end
+
+	def self.all
+		@@all
+	end
+
+	def employees
+		Employee.all.select do |person|
+			person.manager_name = self.name
+		end
+	end
+
+	def hire_employee(name, salary)
+		Employee.new(name, salary, self.name)
+	end
+
+	def self.all_departments
+		@@all.map do |manager|
+			manager.department
+		end 
+	end
+
+	def self.average_age
+		ages = @@all.map do |manager|
+			manager.age
+		end 
+
+		sum = 0.0
+		ages.each do |age|
+			sum += age
+		end
+
+		sum / ages.count
+	end
 end
--- run.rb
+++ run.rb
@@ -4,7 +4,20 @@ require 'pry'
 
 
 #Test your code here
+m1 = Manager.new("John", "Science", 73)
+m2 = Manager.new("Sky", "CSS", 42)
+m3 = Manager.new("Light", "Law", 12)
 
+e1 = Employee.new("Toyz", 100, m1.name)
+e2 = Employee.new("Gray", 90, m1.name)
+e3 = Employee.new("Chicken", 1400, m2.name)
+e4 = Employee.new("NoName", 110, m1.name)
+
+puts "All departments: "
+puts Manager.all_departments
+
+puts "Average Age: "
+puts Manager.average_age
 
 binding.pry
 puts "done"
